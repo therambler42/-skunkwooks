f1fa3f7d87c1a67d37cc25bfda9a8985835c72f9
therambler42 <rambler.glass@yahoo.com>
2025-06-23 18:30:28 -0700

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client'
 //import './index.css'
 import App from './App.jsx'
 
+window.React = React;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
